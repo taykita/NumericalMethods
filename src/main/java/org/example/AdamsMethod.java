c686463f310cbc3fe204749953dc478879c0f6cf
taykita <nordac2001@gmail.com>
2023-03-26 21:26:54 +0300

    интр адамс метод

--- src/main/java/org/example/AdamsMethod.java
+++ src/main/java/org/example/AdamsMethod.java
@@ -21,7 +21,20 @@ public class AdamsMethod {
         }
     }
 
+    /**
+     1,00   1,00   0,00   0,000000
+     1,00   1,00   0,10   0,099344
+     1,00   1,00   0,20   0,196422
+     1,00   1,00   0,30   0,286467
+     1,00   1,00   0,40   0,368090
+     1,00   1,00   0,50   0,441216
+     1,00   1,00   0,60   0,506450
+     1,00   1,00   0,70   0,564634
+     1,00   1,00   0,80   0,616630
+     1,00   1,00   0,90   0,663237
+     1,00   1,00   1,00   0,705157
 
+     */
 
     public static void main(String[] args) {
         double alpha = 1;
@@ -44,9 +57,14 @@ public class AdamsMethod {
         y = y + rungeKuttaMethod.find(step, уравнение, x, y);
         x = x + step;
         System.out.printf("%.2f   %.2f   %.2f   %.6f\n", alpha, beta, x, y);
+        adamsIntra(alpha, beta, step, x, y, b, η, уравнение, xOld, yOld);
+    }
+
+    private static void adamsExtra(double alpha, double beta, double step, double x, double y, double b, double[] η, Уравнение уравнение, double xOld, double yOld) {
+        double delta;
         while (x < b) {
-            η[0] = step*уравнение.f(xOld, yOld);
-            η[1] = step*уравнение.f(x, y);
+            η[0] = step * уравнение.f(xOld, yOld);
+            η[1] = step * уравнение.f(x, y);
             xOld = x;
             yOld = y;
             delta = η[1] - η[0];
@@ -54,6 +72,18 @@ public class AdamsMethod {
             x = x + step;
             System.out.printf("%.2f   %.2f   %.2f   %.6f\n", alpha, beta, x, y);
         }
+    }
 
+    private static void adamsIntra(double alpha, double beta, double step, double x, double y, double b, double[] η, Уравнение уравнение, double xOld, double yOld) {
+        double delta;
+        while (x < b) {
+            η[0] = step * уравнение.f(x, y);
+            double yNew = y + rungeKuttaMethod.find(step, уравнение, x, y);
+            η[1] = step * уравнение.f(x + step, yNew);
+            delta = η[1] - η[0];
+            y = y + η[1] - delta/2;
+            x = x + step;
+            System.out.printf("%.2f   %.2f   %.2f   %.6f\n", alpha, beta, x, y);
+        }
     }
 }
